15e144e47c3ef5043fdb6da5a239c13bcd72af41
jrd <mcfnord@gmail.com>
2021-01-22 04:22:39 +0000

    ok

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -47,51 +47,51 @@ namespace JamFan21.Pages
         {
             using var client = new HttpClient();
             var serverJson = await client.GetStringAsync("http://jamulus.softins.co.uk/servers.php?central=jamulus.fischvolk.de:22124");
-            var servers =  System.Text.Json.JsonSerializer.Deserialize<List<JamulusServers>>(serverJson);
+            var servers = System.Text.Json.JsonSerializer.Deserialize<List<JamulusServers>>(serverJson);
 
-                    IPGeolocationAPI api = new IPGeolocationAPI("7b09ec85eaa84128b48121ccba8cec2a");
+            IPGeolocationAPI api = new IPGeolocationAPI("7b09ec85eaa84128b48121ccba8cec2a");
 
-                    GeolocationParams geoParams = new GeolocationParams();
-                    geoParams.SetIPAddress(HttpContext.Connection.RemoteIpAddress.ToString()) ;
-                    geoParams.SetFields("geo,time_zone,currency");
-                    Geolocation geolocation = api.GetGeolocation(geoParams);
-                    var clientLatitude = Convert.ToDouble( geolocation.GetLatitude() ) ;
-                    var clientLongitude = Convert.ToDouble( geolocation.GetLongitude() );
+            GeolocationParams geoParams = new GeolocationParams();
+            geoParams.SetIPAddress(HttpContext.Connection.RemoteIpAddress.ToString());
+            geoParams.SetFields("geo,time_zone,currency");
+            Geolocation geolocation = api.GetGeolocation(geoParams);
+            var clientLatitude = Convert.ToDouble(geolocation.GetLatitude());
+            var clientLongitude = Convert.ToDouble(geolocation.GetLongitude());
 
-var output = "" ;
-            foreach(var server in servers)
+            var output = "";
+            foreach (var server in servers)
             {
                 // server has people?
-                if(server.clients != null)
+                if (server.clients != null)
                 {
-output = output + " another " ;
+                    output = output + " another ";
                     GeolocationParams geoParamssvr = new GeolocationParams();
                     geoParamssvr.SetIPAddress(server.ipaddrs);
-output = output +  "xxx " + server.ipaddrs.ToString();
+                    output = output + "xxx " + server.ipaddrs;
                     geoParamssvr.SetFields("geo,time_zone,currency");
                     Geolocation geolocationsvr = api.GetGeolocation(geoParamssvr);
-                    double serverLatitude = Convert.ToDouble( geolocationsvr.GetLatitude() );
-                    double serverLongitude = Convert.ToDouble( geolocationsvr.GetLongitude() ) ;
+                    double serverLatitude = Convert.ToDouble(geolocationsvr.GetLatitude());
+                    double serverLongitude = Convert.ToDouble(geolocationsvr.GetLongitude());
 
                     // this is the client's lat-long... ok on server?
-//                    return clientLatitude.ToString() + " " + clientLongitude.ToString() + " " + serverLatitude.ToString() + " " + serverLongitude.ToString()  ;
+                    //                    return clientLatitude.ToString() + " " + clientLongitude.ToString() + " " + serverLatitude.ToString() + " " + serverLongitude.ToString()  ;
 
                     const double EquatorialRadiusOfEarth = 6371D;
                     const double DegreesToRadians = (Math.PI / 180D);
-//                    https://www.simongilbert.net/parallel-haversine-formula-dotnetcore/
-var deltalat = (serverLatitude - clientLatitude) * DegreesToRadians;
-var deltalong = (serverLatitude - clientLatitude) * DegreesToRadians;
-var a = Math.Pow(
-Math.Sin(deltalat /  2D), 2D) +
-Math.Cos(clientLatitude * DegreesToRadians) *
-Math.Cos(serverLatitude * DegreesToRadians) *
-Math.Pow(Math.Sin(deltalong / 2D), 2D);
-var c = 2D * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1D - a));
-var d = EquatorialRadiusOfEarth * c;
-output = output + " " + d.ToString() ;
+                    //                    https://www.simongilbert.net/parallel-haversine-formula-dotnetcore/
+                    var deltalat = (serverLatitude - clientLatitude) * DegreesToRadians;
+                    var deltalong = (serverLatitude - clientLatitude) * DegreesToRadians;
+                    var a = Math.Pow(
+                    Math.Sin(deltalat / 2D), 2D) +
+                    Math.Cos(clientLatitude * DegreesToRadians) *
+                    Math.Cos(serverLatitude * DegreesToRadians) *
+                    Math.Pow(Math.Sin(deltalong / 2D), 2D);
+                    var c = 2D * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1D - a));
+                    var d = EquatorialRadiusOfEarth * c;
+                    output = output + " " + d.ToString();
                 }
             }
-            return output ;
+            return output;
         }
 
         public string RightNow
