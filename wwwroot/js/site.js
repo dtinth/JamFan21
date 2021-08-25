0cf5b981710a6992efb2019e221fedb8a4d959fd
Thai Pangsakulyanont <dtinth@spacet.me>
2021-08-25 21:55:20 +0000

    Copy server IP and port to clipboard on click

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function copyToClipboard(text) {
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+}
